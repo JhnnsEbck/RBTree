b9da06cd19d2ed182f654961df45deee55c58356
Johannes Eiboeck <96199305+JhnnsEbck@users.noreply.github.com>
2024-11-24 23:41:03 +0100

    Code komplett kommentiert

--- src/Main.hs
+++ src/Main.hs
@@ -1,27 +1,24 @@
 module Main where
 
-import System.Environment (getArgs)
-import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
+import System.Environment (getArgs) -- getArgs gibt die Argumente zurück, die dem Programm asu dem Terminal übergeben wurden
+import qualified Data.Text as T -- Data.Text = Bibliothek, die Textverarbeitung ermöglicht
+import qualified Data.Text.IO as TIO -- Data.Text.IO = Bibliothek für lesen und schreiben von Text
 import Data.Char (isAlpha)
-import RBTree (RBTree(..), insert, inorder)
+import RBTree (RBTree(..), insert, inorder) -- importiert die Funktionen aus dem File RBTree.hs
 
 -- Datei-Handling und Tokenisierung
-tokenize :: T.Text -> [T.Text]
-tokenize = T.words 
+tokenize :: T.Text -> [T.Text] -- Funktion tokenize bekommt einen Text und gibt eine Liste von Texten zurück
+tokenize = T.words -- zerlegt Text in Wörter die durch Whitespaces (in dem Fall: Leerzeichen) getrennt sind
         . T.toLower 
-        . T.map (\c -> if c == '\n' then ' ' else c) 
-        . T.filter (\c -> isAlpha c || c == ' ' || c == '\n')
+        . T.map (\c -> if c == '\n' then ' ' else c) -- ersetzt Zeilenumbrüche durch Leerzeichen
+        . T.filter (\c -> isAlpha c || c == ' ' || c == '\n') -- filtert alle Zeichen, die keine Buchstaben, Leerzeichen oder Zeilenumbrüche sind
 
-
--- Tokenize text into words using a regex pattern
--- tokenize :: T.Text -> [T.Text]
--- tokenize text = map T.pack (text =~ "[a-zA-Z']+" :: [String])
-
-
--- Red-Black-Tree mit Wörtern füllen
-buildTree :: [T.Text] -> RBTree T.Text
-buildTree = foldr insert Empty
+-- Wörter mit Red-Black-Tree sortieren
+rbTreeSort :: [T.Text] -> RBTree T.Text -- erhält Liste von T.Text (Wörtern) und gibt einen Red-Black-Tree zurück 
+rbTreeSort = foldr insert Empty 
+-- mit folder wird durch die Liste von Wörtern iteriert 
+-- mit insert (aus RBTree.hs) wird jedes Wort in den Red-Black-Tree eingefügt
+-- mit Empty wird ein leerer Anfangsbaum initialisiert
 
 -- Main Funktion
 main :: IO ()
@@ -29,10 +26,10 @@ main = do
   args <- getArgs
   case args of
     [filePath] -> do
-      content <- TIO.readFile filePath
-      let words = tokenize content
-          tree = buildTree words
-          uniqueSortedWords = inorder tree
+      content <- TIO.readFile filePath -- liest mit TIO.readFile den Inhalt von filePath (der mit getArgs aus dem Terminal übergeben wurde) in content ein
+      let words = tokenize content -- tokenisierung über Funktion "tokenize"
+          tree = rbTreeSort words -- sortierung über Funktion "buildTree"
+          uniqueSortedWords = inorder tree -- traversiert den Baum und gibt die Wörter in sortierter Reihenfolge als Liste zurück
       TIO.writeFile "output.txt" (T.unlines uniqueSortedWords)
-      putStrLn "Output written to output.txt"
+      putStrLn "List of unique words in given text written to output.txt"
     _ -> putStrLn "Usage: <program> <path_to_text_file>"
--- src/RBTree.hs
+++ src/RBTree.hs
@@ -1,33 +1,30 @@
 module RBTree (RBTree(..), insert, inorder) where
 
 -- Red-Black-Tree Datentyp und Funktionen
-data Color = Red | Black deriving (Show, Eq)
-data RBTree a = Empty
-              | Node Color (RBTree a) a (RBTree a)
-              deriving (Show, Eq)
+data Color = Red | Black -- Color ist entweder Red oder Black
+data RBTree a = Empty | Node Color (RBTree a) a (RBTree a) -- initialisiert leeren Baum oder Knoten mit Farbe, linkem Kind, Wert und rechtem Kind
 
 -- Einfügen von Elementen in den Baum
-insert :: Ord a => a -> RBTree a -> RBTree a
-insert x tree = makeBlack (ins tree)
+insert :: Ord a => a -> RBTree a -> RBTree a -- Ord a = a ist eine Instanz von Ord (Ordnung) -> garantiert, dass a vergleichbar ist
+insert x tree = makeBlack (ins tree) -- ins = rekursive Funktion, die Elemente in den Baum einfügt; makeBlack = macht die Wurzel schwarz -> Wurzel nach dem Einfügen ist immer schwarz
   where
-    ins Empty = Node Red Empty x Empty
-    ins (Node color left value right)
-      | x < value = balance color (ins left) value right
-      | x > value = balance color left value (ins right)
-      | otherwise = Node color left value right
-    makeBlack (Node _ left value right) = Node Black left value right
-    makeBlack Empty = Empty
-
-
+    ins Empty = Node Red Empty x Empty -- Wenn Baum leer (ganz am Anfang) -> neuer Knoten mit Farbe Red
+    ins (Node color left value right) -- Wenn Baum nicht leer -> rekursiver Aufruf von ins
+      | x < value = balance color (ins left) value right -- links einfügen
+      | x > value = balance color left value (ins right) -- rechts einfügen
+      | otherwise = Node color left value right -- Wenn x = value -> nichts einfügen (keine Wörter doppelt einfügen)
+    makeBlack (Node _ left value right) = Node Black left value right -- wird aufgerufen, wenn ins fertig ist -> Wurzel schwarz machen
+    makeBlack Empty = Empty -- Wenn Baum leer -> leere Liste
+ 
 -- Balancing
-balance :: Color -> RBTree a -> a -> RBTree a -> RBTree a
-balance Black (Node Red (Node Red a x b) y c) z d = Node Red (Node Black a x b) y (Node Black c z d)
-balance Black (Node Red a x (Node Red b y c)) z d = Node Red (Node Black a x b) y (Node Black c z d)
-balance Black a x (Node Red (Node Red b y c) z d) = Node Red (Node Black a x b) y (Node Black c z d)
-balance Black a x (Node Red b y (Node Red c z d)) = Node Red (Node Black a x b) y (Node Black c z d)
-balance color left value right = Node color left value right
+balance :: Color -> RBTree a -> a -> RBTree a -> RBTree a -- in Funktion balance werden rot-rot Konflikte gelöst
+balance Black (Node Red (Node Red a x b) y c) z d = Node Red (Node Black a x b) y (Node Black c z d) -- links - links -> rotieren
+balance Black (Node Red a x (Node Red b y c)) z d = Node Red (Node Black a x b) y (Node Black c z d) -- links - rechts -> rotieren
+balance Black a x (Node Red (Node Red b y c) z d) = Node Red (Node Black a x b) y (Node Black c z d) -- rechts - links -> rotieren
+balance Black a x (Node Red b y (Node Red c z d)) = Node Red (Node Black a x b) y (Node Black c z d) -- recht - rechts -> rotieren
+balance color left value right = Node color left value right -- Wenn kein rot-rot Konflikt -> nichts machen
 
--- Inorder Traversierung
-inorder :: RBTree a -> [a]
-inorder Empty = []
-inorder (Node _ left value right) = inorder left ++ [value] ++ inorder right
+-- Inorder Baum Traversierung (Rekursiv)
+inorder :: RBTree a -> [a] -- erwartet einen Red-Black-Tree und gibt eine Liste von Werten zurück
+inorder Empty = [] -- Diese Zeile muss hier stehen, weil RBTree a = Empty | Node Color (RBTree a) a (RBTree a) -> Empty ist ein Fall von RBTree a
+inorder (Node _ left value right) = inorder left ++ [value] ++ inorder right -- _ = Farbe des Knotens; inorder traversiert den Baum in der Reihenfolge links - Wurzel - rechts
