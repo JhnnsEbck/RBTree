8d48d361f17064d96687615ef81cb0a6e00693d2
Johannes Eiboeck <96199305+JhnnsEbck@users.noreply.github.com>
2024-11-25 21:12:30 +0100

    ...

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,7 @@ import qualified Data.Text as T -- Data.Text = Bibliothek, die Textverarbeitung 
 import qualified Data.Text.IO as TIO -- Data.Text.IO = Bibliothek für lesen und schreiben von Text
 import Data.Char (isAlpha)
 import RBTree (RBTree(..), insert, inorder) -- importiert die Funktionen aus dem File RBTree.hs
+import Data.Time (getCurrentTime, diffUTCTime)
 
 -- Datei-Handling und Tokenisierung
 tokenize :: T.Text -> [T.Text] -- Funktion tokenize bekommt einen Text und gibt eine Liste von Texten zurück
@@ -15,7 +16,7 @@ tokenize = T.words -- zerlegt Text in Wörter die durch Whitespaces (in dem Fall
 
 -- Wörter mit Red-Black-Tree sortieren
 rbTreeSort :: [T.Text] -> RBTree T.Text -- erhält Liste von T.Text (Wörtern) und gibt einen Red-Black-Tree zurück 
-rbTreeSort = foldr insert Empty 
+rbTreeSort = foldr insert Empty
 -- mit folder wird durch die Liste von Wörtern iteriert 
 -- mit insert (aus RBTree.hs) wird jedes Wort in den Red-Black-Tree eingefügt
 -- mit Empty wird ein leerer Anfangsbaum initialisiert
@@ -23,7 +24,8 @@ rbTreeSort = foldr insert Empty
 -- Main Funktion
 main :: IO ()
 main = do
-  args <- getArgs
+  start <- getCurrentTime -- Ist start hier eine funktion oder variable? 
+  args <- getArgs 
   case args of
     [filePath] -> do
       content <- TIO.readFile filePath -- liest mit TIO.readFile den Inhalt von filePath (der mit getArgs aus dem Terminal übergeben wurde) in content ein
@@ -31,5 +33,6 @@ main = do
           tree = rbTreeSort words -- sortierung über Funktion "buildTree"
           uniqueSortedWords = inorder tree -- traversiert den Baum und gibt die Wörter in sortierter Reihenfolge als Liste zurück
       TIO.writeFile "output.txt" (T.unlines uniqueSortedWords)
-      putStrLn "List of unique words in given text written to output.txt"
-    _ -> putStrLn "Usage: <program> <path_to_text_file>"
+      end <- getCurrentTime
+      putStrLn $ "Execution time: " ++ show (diffUTCTime end start)
+   
